7f388dd93138ae08afd730d1494aa96679fa3bd5
Khanh Nguyen <Khanh13Nguyen13@gmail.com>
2023-11-23 23:37:52 +0100

    Added GA

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import Script from 'next/script'
+
 /**
  * todo list 23/11/23
  * TODO: function href="tel+..."
@@ -7,7 +9,20 @@
  */
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between">
+    <div className="container flex min-h-screen flex-col items-center justify-between">
+
+      {/* <!-- Google tag (gtag.js) --> */}
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-XCBV6CZE0F"></Script>
+      <Script id="google-analytics">
+        {
+          `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-XCBV6CZE0F');
+          `}
+      </Script>
 
     </div>
   )
