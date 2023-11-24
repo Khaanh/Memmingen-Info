f1bdbcd7b4eb9fc3e0998516f0a31914175385ec
Khanh Nguyen <Khanh13Nguyen13@gmail.com>
2023-11-24 22:09:34 +0100

    Add logo

--- app/components/HeaderPage.tsx
+++ app/components/HeaderPage.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import React from 'react';
 import Navigation from './Navigation';
+import Image from 'next/image';
 // import Doctorsdata from './assets/DoctorsData.json'
 
 export default function HeaderPage() {
@@ -8,7 +9,15 @@ export default function HeaderPage() {
     <header className='flex flex-initial text-gray-950 mt-6 mb-14'>
       <div className="container flex-initial mx-auto px-4">
         <nav>
-          <Link className='flex items-center justify-center mb-6' href='/'>Logo</Link>
+          <Link className='flex items-center justify-center mb-6' href='/'>
+            <Image
+              src="/logo-mm_Stadt-Memmingen.png"
+              width={215}
+              height={96}
+              alt="logo Stadt Memmingen"
+              unoptimized
+            />
+          </Link>
           <Navigation />
         </nav>
       </div>
