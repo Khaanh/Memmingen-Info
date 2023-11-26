216ad6979f3842911a4e4e1c6736f860c46a812d
Khanh Nguyen <Khanh13Nguyen13@gmail.com>
2023-11-26 20:53:59 +0100

    done: contact for idea

--- app/doctors/kardiologie/page.tsx
+++ app/doctors/kardiologie/page.tsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 function Kardiologie() {
   return (
-    <div>Kardiologie page</div>
+    <div className='flex flex-col'>
+      <h1 className='text-3xl text-center mb-3 sm:text-5xl'>Извините, данная страница находтся в разработке</h1>
+      <h2 className='text-2xl text-center mb-5 sm:text-4xl'>Своих специалистов или идеи можете предложить: </h2>
+      <div className='flex flex-col justify-center sm:flex-row'>
+        <a href="https://t.me/Kh2nh" className='text-2xl mb-1 break-all text-sky-400 font-medium underline decoration-indigo-500 hover:decoration-wavy'>Telegram: Khanh (Хань)</a>
+        <i className='mx-3 text-2xl'>или</i>
+        <a href="mailto:khanh13nguyen13@gmail.com" className='text-2xl break-all text-sky-400 font-medium underline decoration-indigo-500 hover:decoration-wavy'>Email: khanh13nguyen13@gmail.com</a>
+      </div>
+    </div>
   )
 }
 
